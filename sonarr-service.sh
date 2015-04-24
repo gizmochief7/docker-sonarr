36d54b4b8b0df2c77a5da6752ead8fd5c421653c
Jonathon Leight <jonathon.leight@jleight.com>
2015-04-24 19:19:50 -0400

    Add UrlBase configuration environment variable.

--- sonarr-service.sh
+++ sonarr-service.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
+if [ ! -f "${APP_DATA}/.config/NzbDrone/config.ini" ]; then
+  mkdir -p "${APP_DATA}/.config/NzbDrone"
+  cat <<EOF > "${APP_DATA}/.config/NzbDrone/config.xml"
+<Config>
+  <UrlBase>${SONARR_URLBASE}</UrlBase>
+  <LogLevel>Info</LogLevel>
+  <LaunchBrowser>False</LaunchBrowser>
+</Config>
+EOF
+  chown -R sonarr:sonarr "${APP_DATA}"
+fi
+
 exec /sbin/setuser sonarr /usr/bin/mono /opt/sonarr/NzbDrone.exe --no-browser
